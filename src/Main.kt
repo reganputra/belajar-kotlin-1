c532bf2238b9b10cf90e6590cac300a3384d3e53
reganputra <ramadhanregan334@gmail.com>
2024-08-29 12:35:17 +0700

    first

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+//    Number
+    var age: Byte = 21
+    var egg: Int = 200
+    var number: Float = 31.87F
+    var balance: Long = 1000000L
+
+    var balanceFloat: Float = number.toFloat()
+//    println("Helo")
+    println(balanceFloat)
+    println(age)
+}
